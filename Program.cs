4f25a6ff312c6dfabad35ec8174018030a6e5a3a
Pedro Nakamoto <ph.nakamoto@uol.com.br>
2024-10-26 16:28:20 -0300

    Desafio sistema de celulares concluido com sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Teste com a classe Nokia
+            Console.WriteLine("=== Testando a classe Nokia ===");
+            Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "1111111111", memoria: 64);
+            Console.WriteLine($"Número: {nokia.Numero}, Modelo: {nokia.Modelo}, IMEI: {nokia.Imei}, Memória: {nokia.Memoria}GB");
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            // Teste com a classe Iphone
+            Console.WriteLine("=== Testando a classe Iphone ===");
+            Smartphone iphone = new Iphone(numero: "654321", modelo: "iPhone X", imei: "2222222222", memoria: 128);
+            Console.WriteLine($"Número: {iphone.Numero}, Modelo: {iphone.Modelo}, IMEI: {iphone.Imei}, Memória: {iphone.Memoria}GB");
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
